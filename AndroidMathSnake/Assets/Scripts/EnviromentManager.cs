0c3a421cc133cf59393174dc79d0d618c2ad83a5
Heiko Holz <heiko@whysoseriousgames.de>
2018-10-19 22:55:26 +1000

    BlockySnakes added
    Added the Blocky Snakes packages from Asset Store.

---
+++ AndroidMathSnake/Assets/Scripts/EnviromentManager.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct sMapObjectInfo
+{
+	public sMapObjectInfo(string n, Vector3 p, Vector3 r)
+	{
+		string[] temp = n.Split (new string[]{ "(", " "  }, System.StringSplitOptions.RemoveEmptyEntries);
+			
+		objName = temp != null && temp.Length > 1 ? temp[0] : n;
+
+		pos = p; rot = r;	
+	}
+
+	public string objName;
+	public Vector3 pos;
+	public Vector3 rot;
+}
+
+public class EnviromentManager : MonoBehaviour {
+
+	public GameObject world = null;
+	public GameObject groundParent = null;
+	public GameObject mapObjectsParent = null;
+
+	public bool GUI_ON = true;
+
+	private List<GameObject> grounds = new List<GameObject>();
+	private List<GameObject> mapObjects = new List<GameObject>();
+
+	private int actTheme  = 0;
+
+	void Awake()
+	{
+		for (int i = 0; i < groundParent.transform.childCount; i++)
+		{
+			grounds.Add (groundParent.transform.GetChild (i).gameObject);
+		}
+
+		for (int i = 0; i < mapObjectsParent.transform.childCount; i++)
+		{
+			mapObjects.Add (mapObjectsParent.transform.GetChild (i).gameObject);
+		}
+	}
+
+	private string[] themeNames = {
+		"ASIA",
+		"FOREST",
+		"DESERT",
+		"BALLROOM",
+		"STADIUM",
+		"URBAN"
+	};
+
+	private string[] resNames = {
+		"ThemeAsia",
+		"ThemeForest",
+		"ThemeDesert",
+		"ThemeBallRoom",
+		"ThemeStadium",
+		"ThemeUnknown"
+	};
+
+	private string[] groundResNames = {
+		"Asia_1",
+		"Forest_1",
+		"Desert_1",
+		"BallRoom_1",
+		"Stadium_1",
+		"Unknown_1"
+	};
+
+	const float eButtonWidth = 120;
+	Vector2 btn = new Vector2(0,0);
+	void OnGUI()
+	{
+		if (!GUI_ON)
+			return;
+		for (int i = 0; i < themeNames.Length; i++)
+		{
+			btn.x = (i < 3) ? 10 + (i * (eButtonWidth + 10)) : 10 + ((i-3) * (eButtonWidth + 10));
+			btn.y = (i < 3) ? 40 : 80;
+			if (GUI.Button (new Rect (btn.x, btn.y, eButtonWidth, 40), i == actTheme ? ">>" + themeNames [i] + "<<" : themeNames [i]))
+			{
+				if (!isChanging && actTheme != i)
+				{
+					actTheme = i;
+					changeTheme ();
+				}
+			}
+		}	
+	}
+
+	private bool isChanging = false;
+	private void changeTheme()
+	{
+		isChanging = true;
+		StartCoroutine (_changeTheme());
+	}
+
+
+	IEnumerator _changeTheme()
+	{
+		sMapObjectInfo[] mapObjectInfos = new sMapObjectInfo[mapObjects.Count];
+		for (int i = 0; i < mapObjects.Count; i++)
+		{
+			mapObjectInfos [i] = new sMapObjectInfo (mapObjects [i].gameObject.name, mapObjects [i].transform.position, mapObjects [i].transform.eulerAngles);
+			GameObject.Destroy (mapObjects [i]);
+		}
+		mapObjects.Clear ();
+
+		int groundCount = grounds.Count;
+		Vector3[] groundPos = new Vector3[groundCount];
+		for (int i = 0; i < groundCount; i++)
+		{
+			groundPos [i] = grounds [i].transform.position;
+			GameObject.Destroy (grounds [i]);
+		}
+		grounds.Clear ();
+
+		yield return null;
+		yield return null;
+
+		for (int i = 0; i < groundCount; i++)
+		{
+			Object groundObj = Resources.Load (resNames [actTheme] + "/Ground/" + groundResNames [actTheme]);
+			GameObject groundGO = GameObject.Instantiate (groundObj) as GameObject;
+
+			groundGO.transform.parent = groundParent.transform;
+			groundGO.transform.position = groundPos [i];
+
+			grounds.Add (groundGO);
+		}
+
+		for (int i = 0; i < mapObjectInfos.Length; i++)
+		{
+			Object mapResObj = Resources.Load (resNames [actTheme] + "/" + mapObjectInfos [i].objName);
+			GameObject mapGO = GameObject.Instantiate (mapResObj) as GameObject;
+
+			mapGO.transform.parent = mapObjectsParent.transform;
+			mapGO.transform.position = mapObjectInfos [i].pos;
+			mapGO.transform.eulerAngles = mapObjectInfos [i].rot;
+
+			mapObjects.Add (mapGO);
+		}
+
+		yield return null;
+		isChanging = false;
+	}
+}
---
+++ AndroidMathSnake/Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour {
+	public GameObject sceneWorld;
+	public GameObject plane = null;
+	private int actualParticle = 0;
+	public List<GameObject> particles = new List<GameObject> ();
+	private GameObject particle = null;
+
+	public bool GUI_ON = true;
+
+	// Use this for initialization
+	void Start () {
+		createParticle ();
+	}
+
+	private bool isChange = false;
+	private string[] particleNames = {
+		"EFFECT FOR BUTTERFLY",
+		"EGG EXPLODE",
+		"PICK UP EFFECT",
+		"HIT",
+		"SMOKE - 1",
+		"SMOKE - 2",
+		"TRUCK SMOKE",
+		"WALL CRUSH"
+	};
+
+	void OnGUI()
+	{
+		if (!GUI_ON)
+			return;
+
+		GUIStyle st = new GUIStyle ();
+		st.fontSize = 30;
+		st.alignment = TextAnchor.MiddleCenter;
+		GUI.Label (new Rect(Screen.width * 0.4f, 50, 150, 30), particleNames[actualParticle], st);
+
+		if (GUI.Button(new Rect(10, 175, 150, 30), "PREV") && !isChange)
+		{
+			actualParticle--;
+			if (actualParticle < 0)
+				actualParticle = particles.Count - 1;
+			createParticle ();
+		}
+
+		if (GUI.Button(new Rect(Screen.width-160, 175, 150, 30), "NEXT")&& !isChange)
+		{
+			actualParticle++;
+			if (actualParticle >= particles.Count)
+				actualParticle = 0;
+			createParticle ();
+		}
+
+	}
+
+	private void createParticle()
+	{
+		isChange = true;
+		StartCoroutine (_createParticle ());
+	}
+
+	IEnumerator _createParticle()
+	{
+		if (particle != null)
+			GameObject.Destroy (particle);
+		
+		yield return null;
+
+		particle = GameObject.Instantiate(particles[actualParticle]);
+		particle.transform.parent = sceneWorld.transform;
+		particle.transform.localPosition = new Vector3(0,0,0);
+
+		ParticleSystem[] emitters= particle.GetComponentsInChildren<ParticleSystem>();
+		for (int i = 0; i < emitters.Length; i++) 
+		{
+			ParticleSystem.MainModule m = emitters [i].main;
+			m.loop = true;
+
+			ParticleSystem.CollisionModule cm = emitters [i].collision; 
+			if(cm.enabled)
+				cm.SetPlane(0, plane.transform);
+		}
+
+		yield return null;
+		isChange = false;
+	}
+}
