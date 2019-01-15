3740b6dc3e4bc1c1e131426a8f09c020a4525054
mpiechot <marco.piechotta@student.uni-tuebingen.de>
2019-01-15 13:24:46 +0100

    Create boxplots.R

---
+++ Dokumente/Auswertung/boxplots.R
@@ -0,0 +1,58 @@
+# Kategorie 1 (Challenge)
+challenge_Top <- c(2,2,0,2,2,
+				   2,1,1,4,1,
+				   1,1,4,2,4)
+challenge_Third <- c(1,2,0,4,1,
+					 1,1,0,0,0,
+					 1,1,4,4,4)
+
+# Kategorie 2 (Competence)
+competence_Top <- c(1,4,2,2,3,
+					3,3,1,2,4,
+					4,3,1,0,3)
+competence_Third <- c(3,4,4,4,3,
+					  3,1,2,4,2,
+					  3,4,4,4,2)
+# Kategorie 3 (Flow)
+flow_Top <- c(2,2,1,4,2,
+			  3,3,1,0,3,
+			  1,3,1,0,3)
+flow_Third <- c(1,2,3,0,2,
+				3,4,1,4,3,
+				1,3,3,4,4)
+				
+# Kategorie 4 (Immersion)
+immersion_Top <- c(3,4,4,0,4,
+				   2,2,2,3,
+				   0,2,4,1,1)
+immersion_Third <- c(3,3,4,4,4,
+					 3,2,4,4,3,
+					 1,2,4,4,2)
+					 
+# Kategorie 5 (Negative Effect)
+neg_Top <- c(0,0,0,0,0,
+			 0,0,0,4,0,
+			 0,0,0,0,0)
+neg_Third <- c(0,0,0,0,0,
+			   0,0,1,0,0,
+			   0,2,0,0,0)
+
+# Kategorie 6 (Positive Effect)
+pos_Top <- c(0,1,0,1,4,
+			 4,4,4,0,4,
+			 4,4,4,0,4)
+pos_Third <- c(1,2,0,3,2,
+			   3,3,4,4,4,
+			   4,4,4,4,4)
+
+# Kategorie 7 (Tension)
+tension_Top <- c(0,0,0,0,0,
+				 0,0,0,1,
+				 2,4,0,4,1)
+tension_Third <- c(0,1,0,0,0,
+				   2,1,0,0,0,
+				   1,0,0,0,1)
+				   
+# Steuerungs Frage
+control_Top <- c(1,4,1,0,2)
+control_Third <- c(4,4,4,0,1)
